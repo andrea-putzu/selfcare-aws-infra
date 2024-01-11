615f677205e666b1b04213c2b49849be03949393
FilippoBau <filippo.baudanza@pagopa.it>
2024-01-11 16:10:46 +0100

    added areariservata domain

--- src/main/10-ses.tf
+++ src/main/10-ses.tf
@@ -61,3 +61,29 @@ module "ses_notifichedigitali_it" {
   ]
 
 }
+
+
+module "ses_selfcare_it" {
+  source              = "github.com/pagopa/terraform-aws-ses.git?ref=v1.2.0"
+  domain              = "areariservata.pagopa.it"
+  mail_from_subdomain = "email"
+  aws_region          = var.aws_region
+
+  iam_permissions = [
+    "ses:SendCustomVerificationEmail",
+    "ses:SendEmail",
+    "ses:SendRawEmail",
+    "ses:SendTemplatedEmail"
+  ]
+
+  ses_group_name = "AreariservataSES"
+  user_name      = "Areariservata"
+
+  iam_additional_statements = [
+    {
+      sid       = "Statistics"
+      actions   = ["ses:GetSendQuota"]
+      resources = ["*"]
+    }
+  ]
+  }
